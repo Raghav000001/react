a07725c5aa1396dacbf38bfe0183cd24e0583152
raghav bhardwaj <braghav474@gmail.com>
2026-03-13 11:49:00 +0530

    intro to food app

---
+++ 05_food-app/src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RestroCard from './RestroCard'
+
+const Body = () => {
+  return (
+    <div>
+        body
+        <div>
+            <RestroCard/>
+        </div>
+    </div>
+  )
+}
+
+export default Body
